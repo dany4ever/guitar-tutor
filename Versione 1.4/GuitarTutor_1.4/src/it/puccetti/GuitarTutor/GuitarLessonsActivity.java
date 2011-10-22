bcac440d29a633ca51a1a0233d968a566e69cbec
marcop3 <marcop3@gmail.com>
2011-10-22 18:01:20 +0000

    Versione 1.4

--- Versione 1.4/GuitarTutor_1.4/src/it/puccetti/GuitarTutor/GuitarLessonsActivity.java
+++ Versione 1.4/GuitarTutor_1.4/src/it/puccetti/GuitarTutor/GuitarLessonsActivity.java
@@ -130,14 +130,15 @@ public class GuitarLessonsActivity extends ListActivity implements
 						int position, long id) {
 
 					// DISPATCH ALL'ACTIVITY DEI DETTAGLI
-					Intent poIntent;
+					Intent poIntent = null;
 					String sHtmlFile = (String) m_ListaElementi.get(position).m_sHtml;
 
-					if (sHtmlFile.equalsIgnoreCase("Tetradi")) {
+					if (sHtmlFile.contains(".html")== false) {
 						poIntent = new Intent(view.getContext(),
 								ImageViewer.class);
 						poIntent.putExtra("image", sHtmlFile);
-					} else {
+					} 
+					if(sHtmlFile.contains(".html")) {
 						poIntent = new Intent(view.getContext(),
 								DetailActivity.class);
 						poIntent.putExtra("htmlsource", sHtmlFile);
