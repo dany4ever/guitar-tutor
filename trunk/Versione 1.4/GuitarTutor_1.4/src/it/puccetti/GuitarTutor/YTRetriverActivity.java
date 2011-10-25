18ccb3ac55d5a59cdb64971c651bbef6b859aedb
marcop3@gmail.com <marcop3@gmail.com@c84f9035-d98e-270f-30d9-3c769db5fd4d>
2011-10-25 17:04:55 +0000

    Versione 1.4

--- trunk/Versione 1.4/GuitarTutor_1.4/src/it/puccetti/GuitarTutor/YTRetriverActivity.java
+++ trunk/Versione 1.4/GuitarTutor_1.4/src/it/puccetti/GuitarTutor/YTRetriverActivity.java
@@ -12,36 +12,50 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class YTRetriverActivity extends Activity {
-	
-	private static String YOU_TUBE_HEADER ="http://www.m.youtube.com/";
+
+	private static String YOU_TUBE_HEADER = "http://www.youtube.com/";
 	private static final String QUERY_REQ = "results?search_query=";
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.webview);
 		final Activity activity = this;
-		
+
+		// Adds Progrss bar Support
+		this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
+		 
+		// Makes Progress bar Visible
+		getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
 		WebView webview = (WebView) findViewById(R.id.webview);
-		
-		 webview.setWebChromeClient(new WebChromeClient() {
-			   public void onProgressChanged(WebView view, int progress) {
-			     // Activities and WebViews measure progress with different scales.
-			     // The progress meter will automatically disappear when we reach 100%
-				   //activity.setProgress(progress * 1000);
-			   }
-			 });
-			 webview.setWebViewClient(new GTutorWebViewClient());
+
+		WebSettings wset = webview.getSettings();
+		wset.setJavaScriptEnabled(true);
+		wset.setBlockNetworkLoads(false);
+		wset.setPluginsEnabled(true);
 
 		
-		
+		webview.setWebChromeClient(new WebChromeClient() {
+			public void onProgressChanged(WebView view, int progress) {
+				// Activities and WebViews measure progress with different
+				// scales.
+				// The progress meter will automatically disappear when we reach
+				// 100%
+				if (progress <= 100) {
+					activity.setProgress(progress * 100);
+				}
+			}
+		});
+		webview.setWebViewClient(new GTutorWebViewClient());
+
 		final Button button = (Button) findViewById(R.id.backButton);
 		button.setOnClickListener(buttonBackOnClickListener);
 
 		Bundle b = getIntent().getExtras();
 		String serchString = "";
 		serchString = b.getString("keysearch");
-		webview.loadUrl(YOU_TUBE_HEADER+QUERY_REQ+"chitarra+"
-				+ serchString + "+massimo+varini&aq=f");
+		webview.loadUrl(YOU_TUBE_HEADER + QUERY_REQ + "chitarra+" + serchString
+				+ "+massimo+varini&aq=f");
 	}
 
 	Button.OnClickListener buttonBackOnClickListener = new Button.OnClickListener() {
@@ -57,11 +71,11 @@ public class YTRetriverActivity extends Activity {
 	}
 
 	private class GTutorWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	    view.loadUrl(url);
-	    return true;
-	    }
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
 	}
-	
+
 }
